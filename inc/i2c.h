689ec5adaf6eb4248f65ec0191b012bf29077cce
Grigoriy T <tonkonog.99@gmail.com>
2019-02-28 18:49:50 +0200

    i2c lib 0.01.01

---
+++ inc/i2c.h
@@ -0,0 +1,19 @@
+/**************************************************************************//**
+ * @file     i2c.h
+ * @brief    i2c lib for use
+ * @version  V0.01.01
+ * @date     28. Feb 2019
+ * @copyright
+ * Copyright (C) 2019 Grigoriy T.G.
+ * @note
+ * Writing in progress.
+ *
+ ******************************************************************************/
+#ifndef I2C_H
+#define I2C_H
+
+#include "stm32f10x.h"
+#include "stm32f10x_i2c.h"
+
+
+#endif //I2C_h
